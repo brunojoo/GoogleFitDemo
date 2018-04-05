e0beab11832ed0124ee1313b0c31bf25a05e149f
Bruno <2172321@my.ipleiria.pt>
2018-04-05 14:06:52 +0100

    commit apresentacao

--- app/src/main/java/com/example/bruno/googlefittest/MainActivity.java
+++ app/src/main/java/com/example/bruno/googlefittest/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.bruno.googlefittest;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.renderscript.Element;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,35 +24,22 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.HistoryApi;
 import com.google.android.gms.fitness.data.Bucket;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataReadRequest;
-import com.google.android.gms.fitness.request.StartBleScanRequest;
-import com.google.android.gms.fitness.result.DailyTotalResult;
+import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.fitness.result.DataReadResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.squareup.picasso.Picasso;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -54,20 +47,22 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
-import static com.google.android.gms.fitness.data.Field.FIELD_STEPS;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends  FragmentActivity {
 
     private static final String LOG_TAG = "DEBUGTAG" ;
     private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
     private static final int RC_SIGN_IN = 2;
     private GoogleSignInClient mGoogleSignInClient;
     private SignInButton signInButton;
+
     // Set the dimensions of the sign-in button.
     private GoogleSignInAccount acct;
     private TextView nameAcct;
     private ImageView imgAcct;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,29 +84,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+                                    .requestEmail()
+                                    .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        //esquecer user
-       // mGoogleSignInClient.signOut();
 
-        //dados do ultimo use
+
+
+        //dados do ultimo utilizador
         acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
             setGooglePlusButtonText(signInButton,"Sign out");
             String personName = acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
             Uri personPhoto = acct.getPhotoUrl();
-            System.out.println("Image"+personPhoto);
 
 
             Picasso.get().load(String.valueOf(personPhoto)).transform(new CropSquareTransformation()).into(imgAcct);
             nameAcct.setText(personName);
-
         }
 
 
@@ -158,11 +147,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                //accessGoogleFit();
-            }
-        }
 
         if (requestCode == RC_SIGN_IN) {
             imgAcct.setVisibility(View.VISIBLE);
@@ -172,10 +156,12 @@ public class MainActivity extends AppCompatActivity {
             mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
             setGooglePlusButtonText(signInButton,"Sign out");
             acct = GoogleSignIn.getLastSignedInAccount(this);
-            String personName = acct.getDisplayName();
-            Uri personPhoto = acct.getPhotoUrl();
-            Picasso.get().load(String.valueOf(personPhoto)).transform(new CropSquareTransformation()).into(imgAcct);
-            nameAcct.setText(personName);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                Uri personPhoto = acct.getPhotoUrl();
+                Picasso.get().load(String.valueOf(personPhoto)).transform(new CropSquareTransformation()).into(imgAcct);
+                nameAcct.setText(personName);
+            }
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -183,6 +169,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
@@ -201,17 +188,17 @@ public class MainActivity extends AppCompatActivity {
         cal.setTime(new Date());
         long endTime = cal.getTimeInMillis();
         System.out.println("endtime: "+endTime);
-        cal.add(Calendar.HOUR_OF_DAY, -17);
+        cal.add(Calendar.HOUR_OF_DAY, -60);
         long startTime = cal.getTimeInMillis();
         System.out.println("endtime: "+startTime);
 
 
 
-        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(
+      Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(
                 new DataReadRequest.Builder()
                         .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                         .bucketByTime(1, TimeUnit.DAYS)
-                        .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
+                        .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
                         .build()).
                 addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
             @Override
@@ -225,7 +212,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
+
 
 
 
@@ -234,6 +223,7 @@ public class MainActivity extends AppCompatActivity {
         // If the DataReadRequest object specified aggregated data, dataReadResult will be returned
         // as buckets containing DataSets, instead of just DataSets.
         if (dataReadResult.getBuckets().size() > 0) {
+
             Log.i(
                     LOG_TAG, "Number of returned buckets of DataSets is: " + dataReadResult.getBuckets().size());
             for (Bucket bucket : dataReadResult.getBuckets()) {
@@ -279,8 +269,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
 
 
 
